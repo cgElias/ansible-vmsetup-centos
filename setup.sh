da73c411e164ca27fb3fe5318e2ae932bb63ca59
Elias Tatros <elias.tatros@capgemini.com>
2018-01-04 15:04:24 +0100

    changes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Entrypoint VM Setup routine
 
+while [ "$1" != "" ]; do
+    echo "Parameter 1 equals $1"
+    echo "You now have $# positional parameters"
+
+    case $1 in
+      --skip-git )           skip_git=1
+                            ;;
+      --skip-python )         skip_python=1
+    esac
+
+    # Shift all the parameters down by one
+    shift
+done
+
 # Make sure we are root
 if [[ $EUID -ne 0 ]]; then
   echo "Setup can only be run as root user! Exiting."
@@ -9,25 +23,33 @@ fi
 
 # Update cache and essentials
 echo "Updating cache and preparing.."
-apt-get update
-apt-get install build-essential checkinstall
-apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo apt-get update
+sudo apt-get install build-essential checkinstall -y
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
 
+if [ "$skip_git" != "1"]; then
+# Install git
+echo "Setting up git.."
+sudo apt-get install git -y
+fi
+
+if [ "$skip_python" != "1"]; then
 # Download and Install Python 2.7.14
 echo "Setting up Python.."
 cd /usr/src
 wget https://www.python.org/ftp/python/2.7.14/Python-2.7.14.tgz
 tar xzf Python-2.7.14.tgz
 cd Python-2.7.14
-./configure
-make altinstall
+sudo ./configure
+sudo make altinstall
+fi
 
 # Download and Install Ansible
 echo "Setting up Ansible.."
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update 
+sudo apt-get install ansible -y
 
 # Show distribution and release
 echo "Distribution: "
